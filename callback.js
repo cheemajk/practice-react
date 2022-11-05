2bd4c6ad7ac2e49bc3d6d35a44fe70819f060822
cheemajk <jp1kaur1@gmail.com>
2022-11-05 16:24:21 -0700

    jja

--- callback.js
+++ callback.js
@@ -20,6 +20,6 @@ function genRandom(start, end) {
   }
   console.log(genRandom(1,10)) // random int between 1 to 10
   console.log(genRandom(80,90)) // random int between 80 to 90
-  
+  console.log(genRandom(60,50))
 
   //fun

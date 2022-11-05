010a1aac02148991fb7a4163da9ff972e6bb1274
cheemajk <jp1kaur1@gmail.com>
2022-11-05 15:05:24 -0700

    my first commit

---
+++ firstFile.js
@@ -0,0 +1,29 @@
+
+    function sum(a,b){
+        console.log(a+b);
+    }
+// calling function
+    sum(2,3);
+    sum(4,4);
+    sum(6,6);
+
+
+    function number(num){
+        return num * num;
+    }
+    console.log(number(4));
+
+    //convert Celsius to Fahrenheit
+    function calFahrenheit(cel){
+        return cel * 9/5 + 32; //conversion formula
+    }
+    console.log(calFahrenheit (2));
+    
+    //area of rectangle
+    function area(a,b){
+        return a*b;
+    }
+    console.log(area(10,20));
+
+
+    
